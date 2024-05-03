e85b4a68d157a1c100597a49582637fd2e7f5a02
ravishshardha <90361755+ravishshardha@users.noreply.github.com>
2024-05-02 21:13:26 -0500

    Add files via upload

---
+++ test_model.py
@@ -0,0 +1,41 @@
+import pickle
+
+import cv2
+
+from utils import get_face_landmarks
+
+
+emotions = ['HAPPY', 'SAD', 'SURPRISED']
+
+with open('./model2', 'rb') as f:
+    model = pickle.load(f)
+
+cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+if not cap.isOpened():
+    print("Error: Unable to open camera")
+
+ret, frame = cap.read()
+
+
+while ret:
+    ret, frame = cap.read()
+
+    face_landmarks = get_face_landmarks(frame, draw=True, static_image_mode=False)
+
+    output = model.predict([face_landmarks])
+
+    cv2.putText(frame,
+                emotions[int(output[0])],
+               (10, frame.shape[0] - 1),
+               cv2.FONT_HERSHEY_SIMPLEX,
+               3,
+               (0, 255, 0),
+               5)
+
+    cv2.imshow('frame', frame)
+
+    cv2.waitKey(25)
+
+
+cap.release()
+cv2.destroyAllWindows()
